527b650e66820e3ff56f144ed637c4a619380627
Ece Oezmen <ece.2002.67@gmail.com>
2025-02-19 15:49:54 +0100

    design: new ui in aboutme view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,19 @@
 <script setup>
-import TheSphere from "../components/TheSphere.vue";
-import { aboutmeItems } from "../config/aboutmeItems";
+import StackTabs from "../components/StackTabs.vue";
 </script>
 
 <template>
   <section id="aboutme" class="text-stone-600 h-screen bild flex flex-col">
+    <div class="h-full w-full flex flex-col justify-evenly items-center">
+      <div class="flex flex-col items-center text-6xl">About Me</div>
+
+      <div class="flex justify-around h-2/3">
+        <img
+          src="../assets/img/aboutMe-picture.jpg"
+          class="w-1/4 shadow-2xl rounded-2xl" />
+
+        <StackTabs></StackTabs>
+      </div>
     </div>
   </section>
 </template>
