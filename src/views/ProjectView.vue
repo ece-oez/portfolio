9ffaa3d63ef5b88db6aae88dff41ddc23c111e9c
Ece Oezmen <ece.2002.67@gmail.com>
2025-02-19 15:55:41 +0100

    wip: implement project view

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <section
+    id="skills"
+    class="text-white h-screen background w-full flex flex-col bg-home">
+    <div class="flex flex-col items-center pt-20 text-6xl">Skills</div>
+  </section>
+</template>
+
+<style scoped>
+.background {
+  background-color: #1f2020;
+}
+</style>
