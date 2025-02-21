d7ae082cd794ecc14bf82eeb7877de8b2991912c
Ece Özmen <145540611+ece-oez@users.noreply.github.com>
2025-02-21 07:36:57 +0000

    test: update skills title

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -4,7 +4,7 @@
   <section
     id="skills"
     class="text-white h-screen background w-full flex flex-col bg-home">
-    <div class="flex flex-col items-center pt-20 text-6xl">Skills</div>
+    <div class="flex flex-col items-center pt-20 text-6xl">Skills prod</div>
   </section>
 </template>
 
