fc558b95e1b68b74db188310328ca32bf5dcef3e
Ece Oezmen <ece.2002.67@gmail.com>
2025-02-24 09:05:07 +0100

    responsive navbar

---
+++ src/components/MenuItems.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div>
+    <a href="#aboutme" class="select-none">AboutMe</a>
+    <a href="#projects" class="select-none">Projects</a>
+    <a href="#skills" class="select-none">Skills</a>
+    <a href="#contact" class="select-none">Contact</a>
+  </div>
+</template>
+<style scoped></style>
