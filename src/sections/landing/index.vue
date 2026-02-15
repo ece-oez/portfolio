754f01724a2a6faf09205b554dcb56ba48450214
Ece <145540611+ece-oez@users.noreply.github.com>
2026-02-15 05:01:18 +0000

    Feature: Created a new landing page for the portfolio

---
+++ src/sections/landing/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { useLanguageStore } from "@/stores/language";
+
+const LanguageStore = useLanguageStore();
+</script>
+
+<template>
+
+  <section class="h-screen w-screen flex items-center justify-center flex-col gap-10">
+
+
+    <h1 class="text-[50px] md:text-[80px] font-bold">Hi, I'm 
+      <span class="text-[#FF8C40]">
+Ece
+      </span>
+      </h1>
+    
+  <h3 class="md:text-[#ffa468] text-gray-400 text-center text-[25px] md:font-bold md:text-[40px]">
+  Full Stack Developer & UI/UX Designer
+  </h3>
+
+  <p class="text-center 2xl:w-150 md:w-150 w-100 text-gray-400">
+    I craft beautiful digital experiences that help businesses grow and users thrive. Specialized in Vue.js, Nuxt.js and modern web technologies.
+  </p>
+
+  <div class="flex gap-5">
+<a href="#projects" class="rounded-xl p-3 w-40 bg-[#FF8C40] text-white text-center">View my Work</a>
+<a href="#contact" class="rounded-xl p-3 w-40 border-1 bg-white border-[#e5e5e5] text-center">Get in touch</a>
+
+  </div>
+
+  <div class="flex gap-10 text-[30px] text-gray-400 ">
+    <i class="bi bi-github"></i>
+    <i class="bi bi-envelope"></i>
+  </div>
+
+  </section>
+  
+</template>
+<style scoped>
+
+</style>
