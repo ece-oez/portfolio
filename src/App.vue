ce747a6ca4ff1222de5dcbbf6e72e48c783d9ccc
Ece <145540611+ece-oez@users.noreply.github.com>
2026-02-15 08:24:46 +0000

    Feature:Create a new about page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import landingSection from "./sections/landing/index.vue";
+import aboutSection from "./sections/about/index.vue";
 import { useModalStore } from "@/stores/modal";
 const modalStore = useModalStore();
 </script>
 
 <template>
 <landingSection></landingSection>
+<aboutSection></aboutSection>
 </template>
