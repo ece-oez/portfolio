97a890dbffa73ab9d6d2ebaa263fa59238d3e7cf
Ece <145540611+ece-oez@users.noreply.github.com>
2026-02-16 21:44:38 +0000

    wip

--- src/highlight/me/index.vue
+++ src/highlight/me/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import reel from "@/components/reel/index.vue";
 import { useLanguageStore } from "@/stores/language";
 import { RouterLink } from "vue-router";
 
@@ -6,21 +7,7 @@ const LanguageStore = useLanguageStore();
 </script>
 
 <template>
-  <div
-    class="h-screen w-screen flex flex-row xl:gap-10 bg-cover bg-center bild"
-  >
-    <a
-      class="bg-red-400 w-35 text-center flex items-center justify-center font-bold"
-      href="/#about"
-      >back</a
-    >
-    <button class="w-40 bg-red-500 font-bold">Me</button>
-    <RouterLink
-      class="bg-red-400 w-35 flex items-center justify-center font-bold"
-      to="/me/age"
-      >next</RouterLink
-    >
-  </div>
+  <reel />
 </template>
 <style scoped>
 /* .bild {
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import code from "../highlight/code/index.vue";
+import food from "../highlight/food/index.vue";
+import graduate from "../highlight/graduate/index.vue";
+import job from "../highlight/job/index.vue";
+import life from "../highlight/life/index.vue";
+import pet from "../highlight/pet/index.vue";
+import quotes from "../highlight/quotes/index.vue";
+import travel from "../highlight/travel/index.vue";
 import me from "../highlight/me/index.vue";
-import age from "../highlight/me/age.vue";
-import normalView from "../views/index.vue";
+import normalView from "../views/start/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,9 +24,44 @@ const router = createRouter({
       component: me,
     },
     {
-      path: "/me/age",
-      name: "age",
-      component: age,
+      path: "/code",
+      name: "code",
+      component: code,
+    },
+    {
+      path: "/food",
+      name: "food",
+      component: food,
+    },
+    {
+      path: "/graduate",
+      name: "graduate",
+      component: graduate,
+    },
+    {
+      path: "/job",
+      name: "job",
+      component: job,
+    },
+    {
+      path: "/life",
+      name: "life",
+      component: life,
+    },
+    {
+      path: "/pet",
+      name: "pet",
+      component: pet,
+    },
+    {
+      path: "/quotes",
+      name: "quotes",
+      component: quotes,
+    },
+    {
+      path: "/travel",
+      name: "travel",
+      component: travel,
     },
   ],
 });
